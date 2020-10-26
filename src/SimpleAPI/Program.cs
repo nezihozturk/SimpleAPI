5ea9be8a7c6eaf863b6c5d5beba5e0f3f3413d7f
Nezih Öztürk <nzhzturk@hotmail.com>
2020-10-26 15:39:32 +0100

    program.cs changed

--- src/SimpleAPI/Program.cs
+++ src/SimpleAPI/Program.cs
@@ -11,6 +11,7 @@ namespace SimpleAPI
 {
     public class Program
     {
+        //merhaba
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
